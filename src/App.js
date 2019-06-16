e60785f2c652c00c6ef23d876bde5012ca3139c9
Atsushi Ishida <gipcompany@gmail.com>
2019-06-17 01:30:06 +0900

    implement callbacks with useEffect

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 const App = props => {
   const [state, setState] = useState(props)
   const { name, price } = state
+
+  useEffect(() => {
+    console.log('This is like componentDidMount or componentDidUpdate.')
+  })
+
+  useEffect(() => {
+    console.log('This is like componentDidMount')
+  }, [])
+
+  useEffect(() => {
+    console.log('This callback is for name only.')
+  }, [name])
 
   return (
     <>
