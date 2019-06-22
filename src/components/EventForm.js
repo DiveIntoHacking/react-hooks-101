d75cced75ca55154893846f7b1c28f04220fcb67
Atsushi Ishida <gipcompany@gmail.com>
2019-06-23 05:25:44 +0900

    create operation logs

--- src/components/EventForm.js
+++ src/components/EventForm.js
@@ -2,9 +2,12 @@ import React, { useContext, useState } from 'react'
 
 import {
   CREATE_EVENT,
-  DELETE_ALL_EVENTS
+  DELETE_ALL_EVENTS,
+  ADD_OPERATION_LOG,
+  DELETE_OPERATION_LOGS
 } from '../actions'
 import AppContext from '../contexts/AppContext'
+import { timeCurrentIso8601 } from '../utils'
 
 const EventForm = () => {
   const { state, dispatch } = useContext(AppContext)
@@ -15,9 +18,15 @@ const EventForm = () => {
     e.preventDefault()
 
     dispatch({
-     type: CREATE_EVENT,
-     title,
-     body
+      type: CREATE_EVENT,
+      title,
+      body
+    })
+
+    dispatch({
+      type: ADD_OPERATION_LOG,
+      description: 'イベントを作成しました。',
+      operatedAt: timeCurrentIso8601()
     })
 
     setTitle('')
@@ -27,7 +36,15 @@ const EventForm = () => {
   const deleteAllEvents = e => {
     e.preventDefault()
     const result = window.confirm('全てのイベントを本当に削除しても良いですか？')
-    if (result) dispatch({ type: DELETE_ALL_EVENTS })
+    if (result) {
+      dispatch({ type: DELETE_ALL_EVENTS })
+
+      dispatch({
+        type: ADD_OPERATION_LOG,
+        description: '全てのイベントを削除しました。',
+        operatedAt: timeCurrentIso8601()
+      })
+    }
   }
 
   const unCreatable = title === '' || body === ''
