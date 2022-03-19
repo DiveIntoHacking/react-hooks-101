0684c2beb5761e1c45754bcc836a28da5806caea
Atsushi Ishida <gipcompany@gmail.com>
2022-03-19 12:34:58 +0900

    answer for issue #16889072

--- src/App.js
+++ src/App.js
@@ -4,10 +4,15 @@ const App = props => {
   const [state, setState] = useState(props)
   const { name, price } = state
 
+  const nextState = { ...state }
+  nextState.price = state.price + 1
+
   return (
     <>
       <p>現在の{name}は、{price}円です。</p>
       <button onClick={() => setState({...state, price: price + 1})}>+1</button>
+      <button onClick={() => setState(nextState)}>+1</button>
+      <button onClick={() => setState(prevState => ({ ...prevState, price: prevState.price + 1 }))}>+1</button>
       <button onClick={() => setState({...state, price: price - 1})}>-1</button>
       <button onClick={() => setState(props)}>Reset</button>
       <input value={name} onChange={e => setState({...state, name: e.target.value})}/>
